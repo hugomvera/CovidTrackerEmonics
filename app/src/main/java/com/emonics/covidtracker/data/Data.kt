9adf51abaf23275f56069387eb136f3cc92749e1
hugo <hugomvera@gmail.com>
2023-01-03 19:19:43 -0800

    made dataDao, DataDatabase,DataRepostiotry to not fail

---
+++ app/src/main/java/com/emonics/covidtracker/data/Data.kt
@@ -0,0 +1,36 @@
+package com.emonics.covidtracker.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "data_table")
+data class Data(
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo(name ="data") val date: Int?,
+    @ColumnInfo(name ="dateChecked") val dateChecked: String?,
+    @ColumnInfo(name ="death")val death: Int?
+    //val deathIncrease: Int?,
+   // val hash: String?,
+   // val hospitalized: Int?,
+  //  val hospitalizedCumulative: Int?,
+//    val hospitalizedCurrently: Int,
+//    val hospitalizedIncrease: Int,
+//    val inIcuCumulative: Int,
+//    val inIcuCurrently: Int,
+//    val lastModified: String,
+//    val negative: Int,
+//    val negativeIncrease: Int,
+//    val onVentilatorCumulative: Int,
+//    val onVentilatorCurrently: Int,
+//    val pending: Int,
+//    val posNeg: Int,
+//    val positive: Int,
+//    val positiveIncrease: Int,
+//    val recovered: Any,
+//    val states: Int,
+//    val total: Int,
+//    val totalTestResults: Int,
+  //  val totalTestResultsIncrease: Int?
+//
+)
