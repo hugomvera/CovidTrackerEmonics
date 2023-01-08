5c6f0493206db6b2c4aa451adf38362ab61f8bab
hugo <hugomvera@gmail.com>
2023-01-07 18:05:57 -0800

    linked up login activity button login wich opens the chart graph actiity

--- app/src/main/java/com/emonics/covidtracker/LoginActivity.kt
+++ app/src/main/java/com/emonics/covidtracker/LoginActivity.kt
@@ -2,11 +2,22 @@ package com.emonics.covidtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.loginButon)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, InfoChartPage::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
 
 
 
