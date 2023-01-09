a469b1713e27ed7d2331d4e4942e80d3f8d227e4
nibeditakar <karnibedita22@gmail.com>
2023-01-09 12:51:56 -0500

    check in for login, main,logindetails,sign up activity . Firebase preliminary set done.

---
+++ app/src/main/java/com/emonics/covidtracker/model/UserProfile.kt
@@ -0,0 +1,8 @@
+package com.emonics.covidtracker.model
+
+class UserProfile {
+    var userName: String? = null
+    var emailId: String? = null
+    var password: String? = null
+    var confirmPassword: String? = null
+}
