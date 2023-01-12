7ce7115a02be27691154c6b814a02a5e44f40e84
Amik <amikc1996@gmail.com>
2023-01-12 13:56:02 -0500

    Added working bar chart logic to the bar chart fragment.

--- app/src/main/java/com/emonics/covidtracker/charts/ChartType1Fragment.kt
+++ app/src/main/java/com/emonics/covidtracker/charts/ChartType1Fragment.kt
@@ -6,9 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.emonics.covidtracker.R
+import com.emonics.covidtracker.databinding.ActivityInfoChartPageBinding
+import com.emonics.covidtracker.databinding.FragmentChartType1Binding
 
 
 class ChartType1Fragment : Fragment(R.layout.fragment_chart_type1) {
+
+//    lateinit var binding: FragmentChartType1Binding
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        binding = FragmentChartType1Binding.bind(view)
+//
+//    }
+
+//    fun setLineChartValues(){
+//        val xvalues = ArrayList<String>()
+//            xvalues.add("Monday")
+//            xvalues.add("Tuesday")
+//
+//
+//    }
 
     // TODO: Use bundle/intent in order to retrieve DataViewModel object information.
     // Reminder: Use hardcoded dummy data when implementing charts for the first time inside the
